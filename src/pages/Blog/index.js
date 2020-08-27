83efd799131ac2c52c76ec8046ff730ca337a402
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-08-26 22:17:00 -0300

    Finish blog page

--- src/pages/Blog/index.js
+++ src/pages/Blog/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import {FaTags} from 'react-icons/fa';
 import PageHeader from '../../components/PageHeader';
 
 import {ContainerBlog} from './styles';
@@ -13,7 +14,27 @@ function Blog(){
             />
 
             <ContainerBlog>
-                sdfasd
+                <ul className="post">
+                    <a href="https://medium.com/@patrickvieiramota/programadores-e-suas-maldi%C3%A7%C3%B5es-b888bcddc3ce" target="_blank">
+                        <li className="post">
+                            <div className="img img-post01"></div>
+                            <section>
+                                <div className="tags">
+                                    <FaTags/>
+                                    <p>Code, Programming, Development</p>
+                                </div>
+                                <div className="text">
+                                    <h1>Programadores e suas maldições</h1>
+                                    <p>O primeiro post não poderia ser outro. Sim, vamos falar sobre a famosa maldição do...</p>
+                                </div>
+                                <div className="date">
+                                    <span>Aug 26, 2020 · 1 min read</span>
+                                </div>
+                            </section>
+                        </li>
+                    </a>
+                    
+                </ul>
             </ContainerBlog>
 
         </>
--- src/pages/Blog/styles.js
+++ src/pages/Blog/styles.js
@@ -1,8 +1,93 @@
 import styled from 'styled-components';
 
 export const ContainerBlog = styled.div`
+    width: 80%;
     padding: 0 40px 40px;
 
-    color: white;
+    position: relative;
+    li{
+        display: flex;
+    }
+    .tags{
+        display: flex;
+        align-items: center;
+        color: #93939D;
+        p{
+            margin-left: 10px;
+        }
+    }
+    .text{
+        h1{
+            font-size: 34px;
+            margin: 20px 0;
+            color: white;
+        }
+        p{
+            font-size: 18px;
+            color: #93939D;
+            margin: 0 0 20px;
+        }
+    }
+    .date{
+        span{
+            color: #93939D;
+            position: absolute;
+            bottom: 40px;;
+        }
+    }
+
+    .img{
+        width: 400px;
+        height: 240px;
+        border-radius: 2px;
+    }
+    .img-post01{
+        background: url('https://miro.medium.com/max/700/1*oVYrWwNiJ5FIauPpW1GW9w.jpeg') no-repeat;
+        background-size: cover;
+        margin-right: 30px;
+    }
+
+    @media only screen and (max-width: 1210px){
+        width: 100%;
+        
+        section{
+            width: 50%;
+        }
+    }
+
+    @media only screen and (max-width: 850px){
+        li{
+            display: block;
+        }
+        .img{
+            width: 100%;
+            height: 500px;
+            margin: 0 0 20px;
+        }
+
+        section{
+            width: 100%;
+        }
+    }
+    @media only screen and (max-width: 750px){
+        .img{
+            height: 370px;
+        }
+    }
+    @media only screen and (max-width: 610px){
+        .img{
+            height: 380px;
+        }
+    }
+    @media only screen and (max-width: 550px){
+        .img{
+            height: 280px;
+        }
+    }
+    @media only screen and (max-width: 450px){
+        .img{
+            height: 180px;
+        }
+    }
  
 `;
